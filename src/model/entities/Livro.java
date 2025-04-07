6a0ccdd15c1e5f83ecf29d9b71e6648e9b40ad19
augusto.ludewig <augusto.ludewig@sistemaocepar.coop.br>
2025-04-07 09:10:37 -0300

    commit tentando solucionar erros do super linter

--- src/model/entities/Livro.java
+++ src/model/entities/Livro.java
@@ -3,49 +3,48 @@ package model.entities;
 // Essa classe representa um livro
 
 public class Livro {
-	
-	private String titulo;
-	private String autor;
-	private Integer anoPublicacao;
-	
-	public Livro(String titulo, String autor, Integer anoPublicacao) {
-		this.titulo = titulo;
-		this.autor = autor;
-		this.anoPublicacao = anoPublicacao;
-	}
 
-	public String getTitulo() {
-		return titulo;
-	}
+  private String titulo;
+  private String autor;
+  private Integer anoPublicacao;
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+  public Livro(String titulo, String autor, Integer anoPublicacao) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.anoPublicacao = anoPublicacao;
+  }
 
-	public String getAutor() {
-		return autor;
-	}
+  public String getTitulo() {
+    return titulo;
+  }
 
-	public void setAutor(String autor) {
-		this.autor = autor;
-	}
+  public void setTitulo(String titulo) {
+    this.titulo = titulo;
+  }
 
-	public Integer getAnoPublicacao() {
-		return anoPublicacao;
-	}
+  public String getAutor() {
+    return autor;
+  }
 
-	public void setAnoPublicacao(Integer anoPublicacao) {
-		this.anoPublicacao = anoPublicacao;
-	}
-	
-	// retorna os dados do livro em uma String
-	@Override
-	public String toString() {
-		return "Livro [titulo=" + titulo
-				+ ", autor=" + autor 
-				+ ", anoPublicacao=" + anoPublicacao + "]";
-	}
-	
-	
+  public void setAutor(String autor) {
+    this.autor = autor;
+  }
+
+  public Integer getAnoPublicacao() {
+    return anoPublicacao;
+  }
+
+  public void setAnoPublicacao(Integer anoPublicacao) {
+    this.anoPublicacao = anoPublicacao;
+  }
+
+  // retorna os dados do livro em uma String
+  @Override
+  public String toString() {
+    return "Livro [titulo=" + titulo + ", autor=" + autor + ", anoPublicacao=" + anoPublicacao
+        + "]";
+  }
+
+
 
 }
